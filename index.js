4e3f1afd76cc53c93042bf62c46b2a33e9dee1ed
Andrew Keller <Keller192@gmail.com>
2020-02-11 21:38:04 -0500

    adds logic for multiple promises

--- index.js
+++ index.js
@@ -26,18 +26,38 @@ const getDogPic = async () => {
     const data = await readFilePromise(`${__dirname}/dog.txt`);
     console.log(`Breed: ${data}`);
 
-    const res = await superagent.get(
+    const promise1 = await superagent.get(
       `https://dog.ceo/api/breed/${data}/images/random`
     );
-    console.log(res.body.message);
+    const promise2 = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const promise3 = await superagent.get(
+      `https://dog.ceo/api/breed/${data}/images/random`
+    );
+    const all = await Promise.all([promise1, promise2, promise3]);
+    const images = all.map(item => item.body.message);
+    console.log(images);
 
-    await writeFilePromise('dog-img.txt', res.body.message);
+    await writeFilePromise('dog-img.txt', images.join('\n'));
     console.log('Image saved!');
   } catch (error) {
     console.error(error);
+    throw error;
   }
+  return '2: READY';
 };
-getDogPic();
+
+(async () => {
+  try {
+    console.log('1: Starting dog pic');
+    const x = await getDogPic();
+    console.log(x);
+    console.log('3: Finished dog pic');
+  } catch (error) {
+    console.console.error('ERROR');
+  }
+})();
 
 // promise chain
 
